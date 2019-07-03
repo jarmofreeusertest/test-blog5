2688e789f6a8cc61fdfd967dcfd47dc00499e95f
Jarmo Isotalo <jamo@isotalo.fi>
2019-07-03 16:40:30 +0300

    lol

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,18 @@
  */
 
 // You can delete this file if you're not using it
+
+const { execSync } = require(`child_process`)
+
+const exec = (cmd, ...params) => {
+  console.log(`running: "${cmd}, ${params}"`)
+  const res = execSync(cmd, params)
+  return String(res)
+}
+exports.onPreBuild = function() {
+  console.log(`aw yeah`)
+
+  console.log(exec(`env`))
+
+  console.log(`aw yeah`)
+}
