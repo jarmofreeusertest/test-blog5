813b80677de5c3f55aadc14f3c639f8339ba24aa
jarmofreeusertest <61870539+jarmofreeusertest@users.noreply.github.com>
2020-03-06 17:37:20 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi all!</h1>
-    <p>Welcome to your new Gatsby site branch.</p>
+    <p>Welcome to your new Gatsby site branch1231345
+  .</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
